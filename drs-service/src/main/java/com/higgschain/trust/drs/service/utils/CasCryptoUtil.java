45150b7e0ca1e22dc6fc595819fe6031ecd2c38b
dekuofa1995 <dekuofa1995@gmail.com>
2019-11-19 11:37:05 +0800

    add common util classes

---
+++ drs-service/src/main/java/com/higgschain/trust/drs/service/utils/CasCryptoUtil.java
@@ -0,0 +1,124 @@
+package com.higgschain.trust.drs.service.utils;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.google.common.io.BaseEncoding;
+import com.higgschain.trust.crypto.StacsECKey;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
+
+/**
+ * @author WangQuanzhou
+ * @desc 钱包与Cas Realm对接所需的加密解密接口工具类
+ * @desc 钱包与Cas Realm维护同一套公私钥
+ * @date 2018/2/14 14:52
+ */
+@Slf4j public class CasCryptoUtil {
+
+    public final static int JSON_GENERATE_FEATURES;
+    private static final BaseEncoding HEX = BaseEncoding.base16().lowerCase();
+
+    static {
+        int features = 0;
+        //JSON不做循环引用检测
+        features |= SerializerFeature.DisableCircularReferenceDetect.getMask();
+        //JSON输出NULL属性
+        features |= SerializerFeature.WriteMapNullValue.getMask();
+        //toJSONString的时候对一级key进行按照字母排序
+        features |= SerializerFeature.SortField.getMask();
+        //toJSONString的时候对嵌套结果进行按照字母排序
+        features |= SerializerFeature.MapSortField.getMask();
+        JSON_GENERATE_FEATURES = features;
+    }
+
+    /**
+     * @param message 需要加密的对象
+     * @param priKey  发起方的私钥
+     * @param aesKey  AES秘钥(16bytes长度)
+     * @return CasEncryptRequest  包含requestParam（加密后）和signature字段
+     * @desc 加密方法    先使用发起方的私钥签名   再使用AES加密requestParam
+     */
+    public static CasEncryptRequest encrypt(Object message, String priKey, String aesKey) {
+
+        // 空校验
+        if (null == message) {
+            throw new IllegalArgumentException("obj can not be null !");
+        }
+        if (Strings.isBlank(priKey)) {
+            throw new IllegalArgumentException("priKey can not be null !");
+        }
+        if (Strings.isBlank(aesKey)) {
+            throw new IllegalArgumentException("aesKey can not be null !");
+        }
+
+        CasEncryptRequest casEncryptRequest = new CasEncryptRequest();
+
+        // 使用发起方私钥对requestParam转换成的json字符串进行签名
+        StacsECKey priEcKey = StacsECKey.fromPrivate(HEX.decode(priKey));
+
+        String originMsg =
+            message instanceof String ? (String)message : JSON.toJSONString(message, JSON_GENERATE_FEATURES);
+
+        if (log.isDebugEnabled()) {
+            log.debug("request origin message:{}", originMsg);
+        }
+        casEncryptRequest.setSignature(priEcKey.signMessage(originMsg));
+        String encryptString = AesUtil.encryptToString(originMsg, aesKey);
+        // 使用AES加密requestParam
+        casEncryptRequest.setRequestParam(encryptString);
+
+        return casEncryptRequest;
+    }
+
+    /**
+     * @param resp   请求返回的String对象
+     * @param pubKey 发起方的私钥
+     * @param aesKey aes解密需要的key
+     * @return CasDecryptReponse  解密 验签完成后返回的对象
+     * @desc 接收http请求并进行解密操作
+     */
+    public static CasDecryptResponse decrypt(String resp, String pubKey, String aesKey) {
+        CasDecryptResponse response = new CasDecryptResponse();
+
+        try {
+            JSONObject jsonObject = JSON.parseObject(resp);
+            if (!jsonObject.containsKey("data") || !jsonObject.containsKey("signature") || !jsonObject
+                .containsKey("respCode") || !jsonObject.containsKey("msg")) {
+                throw new RuntimeException("response body format invalid");
+            }
+
+            // 对data进行aes解密操作
+            // 对signature进行验证签名操作
+            String signature = jsonObject.getString("signature");
+            String data = jsonObject.getString("data");
+            String msg = jsonObject.getString("msg");
+            String respCode = jsonObject.getString("respCode");
+
+            String temp = null;
+            if (null != data && null != signature) {
+                temp = AesUtil.decryptToString(data, aesKey);
+                if (temp == null || !StacsECKey.verify(temp, signature, pubKey)) {
+                    // 验证签名失败
+                    log.error("[decrypt]signature verify failed！");
+                    throw new RuntimeException("signature verify failed!");
+                }
+            }
+
+            // 组装返回数据
+            if (null == data) {
+                response.setData(new JSONObject());
+            } else {
+                response.setData(temp);
+            }
+            response.setMsg(msg);
+            response.setRespCode(respCode);
+        } catch (Exception e) {
+            log.error("[decrypt] has error ", e);
+            throw new RuntimeException("decrypt has error", e);
+        }
+
+        return response;
+    }
+
+}
---
+++ drs-service/src/main/java/com/higgschain/trust/drs/service/utils/LambdaExceptionUtil.java
@@ -0,0 +1,138 @@
+package com.higgschain.trust.drs.service.utils;
+
+import java.util.Objects;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * @author dekuofa <br>
+ * @date 2019-11-11 <br>
+ * @see <a href="https://stackoverflow.com/a/27644392/10362153"/>
+ */
+public class LambdaExceptionUtil {
+    /**
+     * .forEach(rethrowConsumer(name -> System.out.println(Class.forName(name)))); or .forEach(rethrowConsumer(ClassNameUtil::println));
+     */
+    public static <T, E extends Exception> Consumer<T> rethrowConsumer(ConsumerWithExceptions<T, E> consumer) throws E {
+        return t -> {
+            try {
+                consumer.accept(t);
+            } catch (Exception exception) {
+                throwAsUnchecked(exception);
+            }
+        };
+    }
+
+    public static <T, U, E extends Exception> BiConsumer<T, U> rethrowBiConsumer(
+        BiConsumerWithExceptions<T, U, E> biConsumer) throws E {
+        return (t, u) -> {
+            try {
+                biConsumer.accept(t, u);
+            } catch (Exception exception) {
+                throwAsUnchecked(exception);
+            }
+        };
+    }
+
+    /**
+     * .map(rethrowFunction(name -> Class.forName(name))) or .map(rethrowFunction(Class::forName))
+     */
+    public static <T, R, E extends Exception> Function<T, R> rethrowFunction(FunctionWithExceptions<T, R, E> function)
+        throws E {
+        return t -> {
+            try {
+                return function.apply(t);
+            } catch (Exception exception) {
+                throwAsUnchecked(exception);
+                return null;
+            }
+        };
+    }
+
+    /**
+     * rethrowSupplier(() -> new StringJoiner(new String(new byte[]{77, 97, 114, 107}, "UTF-8"))),
+     */
+    public static <T, E extends Exception> Supplier<T> rethrowSupplier(SupplierWithExceptions<T, E> function) throws E {
+        return () -> {
+            try {
+                return function.get();
+            } catch (Exception exception) {
+                throwAsUnchecked(exception);
+                return null;
+            }
+        };
+    }
+
+    /**
+     * uncheck(() -> Class.forName("xxx"));
+     */
+    public static void uncheck(RunnableWithExceptions t) {
+        try {
+            t.run();
+        } catch (Exception exception) {
+            throwAsUnchecked(exception);
+        }
+    }
+
+    /**
+     * uncheck(() -> Class.forName("xxx"));
+     */
+    public static <R, E extends Exception> R uncheck(SupplierWithExceptions<R, E> supplier) {
+        try {
+            return supplier.get();
+        } catch (Exception exception) {
+            throwAsUnchecked(exception);
+            return null;
+        }
+    }
+
+    /**
+     * uncheck(Class::forName, "xxx");
+     */
+    public static <T, R, E extends Exception> R uncheck(FunctionWithExceptions<T, R, E> function, T t) {
+        try {
+            return function.apply(t);
+        } catch (Exception exception) {
+            throwAsUnchecked(exception);
+            return null;
+        }
+    }
+
+    @SuppressWarnings("unchecked") private static <E extends Throwable> void throwAsUnchecked(Exception exception)
+        throws E {
+        throw (E)exception;
+    }
+
+    @FunctionalInterface public interface ConsumerWithExceptions<T, E extends Exception> {
+        void accept(T t) throws E;
+    }
+
+    @FunctionalInterface public interface BiConsumerWithExceptions<T, U, E extends Exception> {
+        void accept(T t, U u) throws E;
+    }
+
+    @FunctionalInterface public interface FunctionWithExceptions<T, R, E extends Exception> {
+        R apply(T t) throws E;
+
+        default <V> FunctionWithExceptions<T, V, E> andThen(Function<R, V> after) throws E {
+            Objects.requireNonNull(after);
+            return (T t) -> after.apply(rethrowFunction(this).apply(t));
+        }
+
+        default ConsumerWithExceptions<T, E> consumer(Consumer<R> after) throws E {
+            Objects.requireNonNull(after);
+            return (T t) -> after.accept(rethrowFunction(this).apply(t));
+        }
+    }
+
+    @FunctionalInterface public interface SupplierWithExceptions<T, E extends Exception> {
+        T get() throws E;
+    }
+
+    @FunctionalInterface public interface RunnableWithExceptions<E extends Exception> {
+        void run() throws E;
+    }
+
+}
