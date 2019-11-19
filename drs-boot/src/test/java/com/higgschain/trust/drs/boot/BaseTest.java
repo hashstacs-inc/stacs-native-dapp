4f9a82a35a8eb60bc473988292446268f1852f29
dekuofa1995 <dekuofa1995@gmail.com>
2019-11-19 11:34:53 +0800

    :heavy_minus_sign: Removing a dependency. remove sdk :heavy_plus_sign: Adding a dependency. add crypto

--- drs-boot/src/test/java/com/higgschain/trust/drs/boot/BaseTest.java
+++ drs-boot/src/test/java/com/higgschain/trust/drs/boot/BaseTest.java
@@ -2,7 +2,7 @@ package com.higgschain.trust.drs.boot;
 
 import com.alibaba.fastjson.JSON;
 import com.alipay.sofa.ark.springboot.runner.ArkBootRunner;
-import com.hashstacs.sdk.crypto.GspECKey;
+import com.higgschain.trust.crypto.StacsECKey;
 import com.higgschain.trust.drs.model.callback.TransactionReceipt;
 import com.higgschain.trust.drs.model.callback.TxReceiptData;
 import com.higgschain.trust.drs.service.model.TxCallbackBO;
@@ -21,7 +21,6 @@ import java.util.List;
 
 /**
  * @author liuyu
- * @description
  * @date 2019-11-07
  */
 @RunWith(ArkBootRunner.class)
@@ -35,7 +34,7 @@ public class BaseTest {
         TxRequestBO bo = new TxRequestBO();
         bo.setTxId("tx_id_test_" + System.currentTimeMillis());
         bo.setPolicyId("ISSUE");
-        GspECKey ecKey = new GspECKey();
+        StacsECKey ecKey = new StacsECKey();
         bo.setSubmitter(ecKey.getHexAddress());
         bo.setBdCode("REGIST_POLICY");
         bo.setFuncName("test()");
