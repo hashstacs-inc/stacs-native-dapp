c3ebdf383f5264293a439c35c355dc04c494de90
lichengcai <lichengcai@primeledger.cn>
2019-11-26 10:28:31 +0800

    remove docker build

--- bin/deploy-snapshot.sh
+++ bin/deploy-snapshot.sh
@@ -9,7 +9,7 @@ elif [[ $TRAVIS_BRANCH == master ]] || \
    [[ $TRAVIS_BRANCH == release* ]];
 then
   echo -e "Publishing maven snapshot...\n"
-  mvn -s bin/settings.xml deploy -DskipTests -Dmaven.install.skip=true -Ddockerfile.username=$DOCKER_USERNAME -Ddockerfile.password=$DOCKER_PASSWORD -B
+  mvn -s bin/settings.xml deploy -DskipTests -Dmaven.install.skip=true -B
   echo -e "Published maven snapshot"
 else
   echo -e "branch $TRAVIS_BRANCH no need publish"
