f80933a1b6788b468737d53c076b5b3737899a09
dekuofa1995 <dekuofa1995@gmail.com>
2019-11-28 15:17:24 +0800

    :art: format of the code.

--- dapp-maven-plugin/src/main/java/io/stacs/nav/dapp/maven/mojo/utils/WriteHelper.java
+++ dapp-maven-plugin/src/main/java/io/stacs/nav/dapp/maven/mojo/utils/WriteHelper.java
@@ -2,22 +2,18 @@ package io.stacs.nav.dapp.maven.mojo.utils;
 
 import com.alipay.sofa.ark.common.util.FileUtils;
 import com.alipay.sofa.ark.tools.JarWriter;
-import io.stacs.nav.dapp.maven.mojo.utils.LambdaExceptionUtil.BiConsumerWithExceptions;
 
 import javax.annotation.Nonnull;
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.function.Function;
-import java.util.function.Supplier;
 
 import static com.alipay.sofa.ark.spi.constant.Constants.ARK_CONF_BASE_DIR;
 import static com.alipay.sofa.ark.spi.constant.Constants.ARK_CONF_FILE;
-import static io.stacs.nav.dapp.maven.mojo.Constants.APPLICATION_PROPERTIES;
-import static io.stacs.nav.dapp.maven.mojo.Constants.DRS.*;
+import static io.stacs.nav.dapp.maven.mojo.Constants.DRS.ARK_CONFIG_KEY;
 import static java.io.File.separator;
 
 /**
@@ -50,62 +46,43 @@ public class WriteHelper {
                 new File(confDir.getAbsolutePath() + separator + "ark" + separator + ARK_CONF_FILE)));
     }
 
-    public static File buildPropertiesFile(File confDir, Supplier<Properties> newProp,
-        Function<Properties, Properties> buildProperties) throws IOException {
-        Path path = Files.createTempFile("resource", "temp");
-        Properties prop = newProp.get();
-        File temp = path.toFile();
-        if (confDir.exists() && confDir.isDirectory()) {
-            File source = new File(confDir, "drs.properties");
-            if (source.exists()) {
-                FileInputStream in = new FileInputStream(source);
-                java.nio.file.Files.copy(in, path, StandardCopyOption.REPLACE_EXISTING);
-                prop.load(new FileInputStream(temp));
+    public static Function<File, Optional<File>> createTempFile() {
+        return file -> {
+            try {
+                return Optional.of(Files.createTempFile("resource-", ".tmp").toFile());
+            } catch (IOException e) {
+                return Optional.empty();
             }
-        }
-
-        buildProperties.apply(prop);
-        writePropertiesToFile().accept(prop, temp);
-        return temp;
-    }
-
-    public static Function<Properties, Properties> drsPropertiesBuilder() {
-        return prop -> {
-            prop.setProperty(DRS_CALL_BACK_URL_KEY, DRS_CALL_BACK_URL_VALUE);
-            return prop;
         };
     }
 
-    public static BiConsumerWithExceptions<Properties, File, IOException> writePropertiesToFile() {
-        return (prop, targetFile) -> prop.store(new FileOutputStream(targetFile), null);
+    public static Function<File, Optional<Properties>> loadPropFromFile() {
+        return file -> {
+            Properties prop = new Properties();
+            try {
+                prop.load(new FileInputStream(file));
+                return Optional.of(prop);
+            } catch (IOException e) {
+                return Optional.empty();
+            }
+        };
+    }
+
+    public static Function<Optional<Properties>, Optional<Properties>> buildProperties() {
+        return opt -> opt.map(prop -> {
+            prop.setProperty(ARK_CONFIG_KEY, ARK_CONFIG_KEY);
+            return opt;
+        }).orElse(opt);
     }
 
     private static InputStream buildArkConfInputStream(@Nonnull File configFile) throws IOException {
-        // todo 优化
-        Properties prop = new Properties();
-        Path temp = java.nio.file.Files.createTempFile("resource-", ".tmp");
-        if (configFile.exists()) {
-            FileInputStream in = new FileInputStream(configFile);
-            java.nio.file.Files.copy(in, temp, StandardCopyOption.REPLACE_EXISTING);
-            prop.load(in);
-
-        } else {
-            InputStream inStream = WriteHelper.class.getClassLoader().getResourceAsStream(ARK_CONF_FILE);
-            assert inStream != null;
-            java.nio.file.Files.copy(inStream, temp, StandardCopyOption.REPLACE_EXISTING);
-
-        }
-        prop.setProperty(ARK_CONFIG_KEY, ARK_CONFIG_KEY);
-        prop.store(new FileOutputStream(temp.toFile()), null);
-
-        return new FileInputStream(temp.toFile());
+        File temp = createTempFile().apply(configFile)
+            .orElseThrow(() -> new IOException("can't found/access file:" + configFile.getAbsolutePath()));
+        Properties prop = loadPropFromFile().andThen(buildProperties()).apply(temp)
+            .orElseThrow(() -> new FileNotFoundException(configFile.getAbsolutePath()));
+        prop.store(new FileOutputStream(temp), null);
+        return new FileInputStream(temp);
     }
 
-    public static void buildPropertiesFile(File bizDir, JarWriter jarWriter) throws IOException {
-
-        // write boot config
-        jarWriter.writeEntry(FileUtils.getCompatiblePath(ARK_CONF_BASE_DIR + separator + ARK_CONF_FILE),
-            buildArkConfInputStream(new File(bizDir.getAbsolutePath() + separator + APPLICATION_PROPERTIES)));
-    }
 
 }
