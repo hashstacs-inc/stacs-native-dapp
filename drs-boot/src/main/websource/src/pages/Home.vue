691bab3396216297faa6638bc5c15dccf6220e58
chenyi <1240513273@qq.com>
2019-11-28 14:29:53 +0800

    feat:创建web源代码

---
+++ drs-boot/src/main/websource/src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>首页</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HOME',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+h1 {
+  text-align: center;
+}
+</style>
