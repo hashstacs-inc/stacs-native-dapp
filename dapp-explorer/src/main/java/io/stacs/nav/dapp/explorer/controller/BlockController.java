8c1b741233f4e6b89f5e79594c47231c3f8242ff
dekuofa1995 <dekuofa1995@gmail.com>
2019-12-03 10:53:56 +0800

    dapp change

--- dapp-explorer/src/main/java/io/stacs/nav/dapp/explorer/controller/BlockController.java
+++ dapp-explorer/src/main/java/io/stacs/nav/dapp/explorer/controller/BlockController.java
@@ -1,17 +1,47 @@
 package io.stacs.nav.dapp.explorer.controller;
 
+import com.alipay.sofa.ark.spi.service.ArkInject;
+import io.stacs.nav.drs.api.IQueryService;
 import io.stacs.nav.drs.api.model.RespData;
+import io.stacs.nav.drs.api.model.block.BlockVO;
+import io.stacs.nav.drs.api.model.query.QueryBlockByHeightVO;
+import io.stacs.nav.drs.api.model.query.QueryBlockVO;
+import io.stacs.nav.drs.api.model.query.QueryTxListVO;
+import io.stacs.nav.drs.api.model.query.QueryTxVO;
+import io.stacs.nav.drs.api.model.tx.CoreTransactionVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static io.stacs.nav.drs.api.model.RespData.success;
 
 /**
  * @author dekuofa <br>
  * @date 2019-12-02 <br>
  */
-@RestController @RequestMapping("/block") @Slf4j public class BlockController {
+@SuppressWarnings("unchecked") @RestController @RequestMapping("/block") @Slf4j public class BlockController {
 
-    public RespData<?> queryCurrentHeight() {
-        return null;
+    @ArkInject private IQueryService queryService;
+
+    public RespData<Long> queryCurrentHeight() {
+        return success(queryService.queryCurrentHeight());
+    }
+
+    public RespData<List<CoreTransactionVO>> queryCoreTxListByPage(QueryTxListVO vo) {
+        return success(queryService.queryCoreTxListByPage(vo));
+    }
+
+    public RespData<CoreTransactionVO> queryCoreTxById(QueryTxVO vo) {
+        return success(queryService.queryCoreTxById(vo));
+    }
+
+    public RespData<List<BlockVO>> queryBlockListByPage(QueryBlockVO vo) {
+        return success(queryService.queryBlockListByPage(vo));
+    }
+
+    public RespData<BlockVO> queryBlockByHeight(QueryBlockByHeightVO vo) {
+        return success(queryService.queryBlockByHeight(vo));
     }
 }
--- drs-api/src/main/java/io/stacs/nav/drs/api/IQueryService.java
+++ drs-api/src/main/java/io/stacs/nav/drs/api/IQueryService.java
@@ -2,6 +2,14 @@ package io.stacs.nav.drs.api;
 
 import io.stacs.nav.drs.api.exception.DappException;
 import io.stacs.nav.drs.api.model.BaseTxVO;
+import io.stacs.nav.drs.api.model.block.BlockVO;
+import io.stacs.nav.drs.api.model.query.QueryBlockByHeightVO;
+import io.stacs.nav.drs.api.model.query.QueryBlockVO;
+import io.stacs.nav.drs.api.model.query.QueryTxListVO;
+import io.stacs.nav.drs.api.model.query.QueryTxVO;
+import io.stacs.nav.drs.api.model.tx.CoreTransactionVO;
+
+import java.util.List;
 
 /**
  * @author dekuofa1995
@@ -13,5 +21,15 @@ public interface IQueryService {
      * generate sign value
      */
     String generateSignature(BaseTxVO vo) throws DappException;
+
+    Long queryCurrentHeight();
+
+    List<CoreTransactionVO> queryCoreTxListByPage(QueryTxListVO vo);
+
+    CoreTransactionVO queryCoreTxById(QueryTxVO vo);
+
+    List<BlockVO> queryBlockListByPage(QueryBlockVO vo);
+
+    BlockVO queryBlockByHeight(QueryBlockByHeightVO vo);
 
 }
