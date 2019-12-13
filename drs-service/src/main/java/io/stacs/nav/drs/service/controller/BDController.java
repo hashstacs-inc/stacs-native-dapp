e1c3fdc57924994c421e9931cecd28de02ed715f
liuyu <11871958@qq.com>
2019-12-13 11:51:43 +0800

    fix bugs

--- drs-service/src/main/java/io/stacs/nav/drs/service/controller/BDController.java
+++ drs-service/src/main/java/io/stacs/nav/drs/service/controller/BDController.java
@@ -1,14 +1,20 @@
 package io.stacs.nav.drs.service.controller;
 
+import io.stacs.nav.crypto.StacsECKey;
 import io.stacs.nav.drs.api.exception.DappError;
 import io.stacs.nav.drs.api.exception.DappException;
 import io.stacs.nav.drs.api.model.RespData;
 import io.stacs.nav.drs.service.service.BDService;
 import io.stacs.nav.drs.service.vo.BDVO;
+import io.stacs.nav.drs.service.vo.GenSignVO;
 import io.stacs.nav.drs.service.vo.SignVO;
 import lombok.extern.slf4j.Slf4j;
+import org.spongycastle.util.encoders.Hex;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author suimi
@@ -17,6 +23,21 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/bd") @RestController @Slf4j public class BDController {
 
     @Autowired BDService bdService;
+
+    @PostMapping("/sign") public RespData sign(@RequestBody GenSignVO vo) {
+        try {
+            SignVO signVO = bdService.generateSignature(vo.getBdvo());
+            String s = StacsECKey.fromPrivate(Hex.decode(vo.getPriKey())).signMessage(signVO.getSign());
+            return RespData.success(s);
+        } catch (DappException e) {
+            log.error("[sign]has dapp error", e);
+            return RespData.fail(e);
+        } catch (Throwable e) {
+            log.error("[sign]has unknown error", e);
+            return RespData.fail(DappError.DAPP_COMMON_ERROR);
+        }
+    }
+
 
     @PostMapping("/genSign") public RespData genSign(@RequestBody BDVO bdvo) {
         try {
