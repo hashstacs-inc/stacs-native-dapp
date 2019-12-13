baaa9c756c82f5f2f40fd8d01254e9e50dd3ce7b
liuyu <11871958@qq.com>
2019-12-13 16:39:20 +0800

    fix bugs

--- drs-service/src/main/java/io/stacs/nav/drs/service/service/SignatureService.java
+++ drs-service/src/main/java/io/stacs/nav/drs/service/service/SignatureService.java
@@ -1,11 +1,16 @@
 package io.stacs.nav.drs.service.service;
 
+import com.alibaba.fastjson.JSON;
 import io.stacs.nav.drs.api.ISignatureService;
 import io.stacs.nav.drs.api.exception.DappException;
 import io.stacs.nav.drs.api.model.BaseTxVO;
 import io.stacs.nav.drs.api.model.bd.BusinessDefine;
 import io.stacs.nav.drs.api.model.bd.FunctionDefine;
+import io.stacs.nav.drs.service.dao.BusinessDefineDao;
+import io.stacs.nav.drs.service.dao.po.BusinessDefinePO;
+import io.stacs.nav.drs.service.utils.BeanConvertor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,10 +27,17 @@ import static io.stacs.nav.drs.api.exception.DappError.FUNCTION_NOT_FIND_ERROR;
 @Slf4j @Service public class SignatureService implements ISignatureService {
 
     @Autowired BlockChainService bdService;
+    @Autowired BusinessDefineDao businessDefineDao;
 
     @Override public String generateSignature(BaseTxVO vo) throws DappException {
         String execPolicyId;
-        BusinessDefine bd = bdService.queryBDByCode(vo.getBdCode());
+        BusinessDefinePO po = businessDefineDao.queryBDByCode(vo.getBdCode());
+        log.info("[generateSignature]query BD from database:{}",po);
+        BusinessDefine bd = BeanConvertor.convertBean(po, BusinessDefine.class);
+        if (bd != null && StringUtils.isNotEmpty(po.getFunctions())) {
+            List<FunctionDefine> functionDefines = JSON.parseArray(po.getFunctions(), FunctionDefine.class);
+            bd.setFunctions(functionDefines);
+        }
         if (CREATE_CONTRACT.getFunctionName().equals(vo.getFunctionName())) {
             execPolicyId = bd.getInitPolicy();
         } else {
