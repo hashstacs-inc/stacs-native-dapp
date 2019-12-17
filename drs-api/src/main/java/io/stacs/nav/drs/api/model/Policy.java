9684611dc921e361a3e4a89e8cad48fe54b48483
liuyu <11871958@qq.com>
2019-12-17 17:26:50 +0800

    modify

--- drs-api/src/main/java/io/stacs/nav/drs/api/model/Policy.java
+++ drs-api/src/main/java/io/stacs/nav/drs/api/model/Policy.java
@@ -1,7 +1,13 @@
 package io.stacs.nav.drs.api.model;
 
+import io.stacs.nav.drs.api.enums.CallbackTypeEnum;
+import io.stacs.nav.drs.api.enums.DecisionTypeEnum;
+import io.stacs.nav.drs.api.enums.VotePatternEnum;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 /**
  * The type Policy.
@@ -21,4 +27,44 @@ import lombok.Setter;
      * policy name
      */
     private String policyName;
+
+    /**
+     * domain id of related to policy
+     */
+    private List<String> domainIds;
+    /**
+     * the decision type for vote ,1.FULL_VOTE,2.ONE_VOTE,3.ASSIGN_NUM
+     */
+    private DecisionTypeEnum decisionType;
+
+    /**
+     * rs vote pattern 1.SYNC 2.ASYNC
+     */
+    @NotNull private VotePatternEnum votePattern;
+
+    /**
+     * callback type of slave 1.ALL 2.SELF
+     */
+    @NotNull private CallbackTypeEnum callbackType;
+    /**
+     * the number to verify
+     */
+    private int verifyNum;
+    /**
+     * domain-ids that must be verified
+     */
+    private List<String> mustDomainIds;
+    /**
+     * the expression for vote rule
+     * example: n/2+1
+     */
+    private String expression;
+    /**
+     * An authorized domain-ids collection is required for modification
+     */
+    private List<String> requireAuthIds;
+    /**
+     * version of policy,default 0
+     */
+    private int version;
 }
