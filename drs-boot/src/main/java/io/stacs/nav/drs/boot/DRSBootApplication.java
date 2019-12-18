1f6264c5b2ab573ec2761788d91c206cfb551754
dekuofa1995 <dekuofa1995@gmail.com>
2019-12-18 14:30:03 +0800

    fix bug

--- drs-boot/src/main/java/io/stacs/nav/drs/boot/DRSBootApplication.java
+++ drs-boot/src/main/java/io/stacs/nav/drs/boot/DRSBootApplication.java
@@ -34,8 +34,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class DRSBootApplication {
 
     public static void main(String[] args) {
-        String[] params = new String[args.length + 1];
+        String[] params = new String[args.length + 2];
         params[0] = "--spring.config.name=boot";
+        params[args.length + 1] = "--spring.config.location=classpath:*";
         System.arraycopy(args, 0, params, 1, args.length);
 
         SpringApplication.run(DRSBootApplication.class, params);
