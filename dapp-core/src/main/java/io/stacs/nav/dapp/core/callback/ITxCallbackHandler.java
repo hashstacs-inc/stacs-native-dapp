0d6d8675d11b57e15a988e1b1593b586e5fda561
liuyu <11871958@qq.com>
2019-12-18 18:08:31 +0800

    modify`

--- dapp-core/src/main/java/io/stacs/nav/dapp/core/callback/ITxCallbackHandler.java
+++ dapp-core/src/main/java/io/stacs/nav/dapp/core/callback/ITxCallbackHandler.java
@@ -22,7 +22,7 @@ public interface ITxCallbackHandler {
      * @return
      */
     default String supportVersion() {
-        return VersionEnum.V4.getCode();
+        return VersionEnum.V1.getCode();
     }
     /**
      *
