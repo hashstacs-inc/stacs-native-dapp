0c0d413042a1311dd171111392ac32eb023835d3
liuyu <11871958@qq.com>
2019-12-19 12:22:46 +0800

    modify`

--- drs-service/src/main/java/io/stacs/nav/drs/service/service/TxRequestService.java
+++ drs-service/src/main/java/io/stacs/nav/drs/service/service/TxRequestService.java
@@ -2,9 +2,11 @@ package io.stacs.nav.drs.service.service;
 
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
+import io.stacs.nav.drs.api.enums.ApiConstants;
 import io.stacs.nav.drs.api.exception.DappError;
 import io.stacs.nav.drs.api.exception.DappException;
 import io.stacs.nav.drs.api.model.BaseTxVO;
+import io.stacs.nav.drs.api.model.RespData;
 import io.stacs.nav.drs.api.model.bd.BusinessDefine;
 import io.stacs.nav.drs.api.model.bd.FunctionDefine;
 import io.stacs.nav.drs.service.dao.BusinessDefineDao;
@@ -14,6 +16,7 @@ import io.stacs.nav.drs.service.dao.po.TxRequestPO;
 import io.stacs.nav.drs.service.enums.RequestStatus;
 import io.stacs.nav.drs.service.model.TxRequestBO;
 import io.stacs.nav.drs.service.network.BlockChainFacade;
+import io.stacs.nav.drs.service.network.BlockChainHelper;
 import io.stacs.nav.drs.service.scheduler.InitTxDisruptor;
 import io.stacs.nav.drs.service.utils.BeanConvertor;
 import io.stacs.nav.drs.service.vo.PermissionCheckVO;
@@ -29,6 +32,7 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
+import static io.stacs.nav.drs.api.enums.ApiConstants.QueryApiEnum.CHECK_PERMISSION;
 import static io.stacs.nav.drs.api.enums.ApiConstants.TransactionApiEnum.CONTRACT_INVOKER;
 import static io.stacs.nav.drs.api.enums.ApiConstants.TransactionApiEnum.CREATE_CONTRACT;
 import static io.stacs.nav.drs.api.exception.DappError.BD_NOT_FIND_ERROR;
@@ -49,6 +53,7 @@ import static io.stacs.nav.drs.api.exception.DappException.newError;
     @Autowired TxRequestDao txRequestDao;
     @Autowired InitTxDisruptor initTxDisruptor;
     @Autowired BlockChainFacade blockChainFacade;
+    @Autowired BlockChainHelper blockChainHelper;
     @Autowired BlockChainService blockChainService;
     @Autowired BusinessDefineDao businessDefineDao;
 
@@ -61,9 +66,9 @@ import static io.stacs.nav.drs.api.exception.DappException.newError;
         log.info("[submitTx]is start,{}", JSON.toJSONString(vo));
         //query business define by bdCode
         BusinessDefinePO po = businessDefineDao.queryBDByCode(vo.getBdCode());
-        log.info("[submit]query BD from database:{}",po);
-        if(po == null){
-            log.warn("[submit]bd is not exists,bdCode:{}",vo.getBdCode());
+        log.info("[submit]query BD from database:{}", po);
+        if (po == null) {
+            log.warn("[submit]bd is not exists,bdCode:{}", vo.getBdCode());
             throw new DappException(BD_NOT_FIND_ERROR);
         }
         BusinessDefine bd = BeanConvertor.convertBean(po, BusinessDefine.class);
@@ -80,8 +85,8 @@ import static io.stacs.nav.drs.api.exception.DappException.newError;
             execPermission = bd.getInitPermission();
         } else {
             List<FunctionDefine> functions = bd.getFunctions();
-            Optional<FunctionDefine> define = functions.stream().filter(a -> a.getName().equals(vo.getFunctionName()))
-                .findFirst();
+            Optional<FunctionDefine> define =
+                functions.stream().filter(a -> a.getName().equals(vo.getFunctionName())).findFirst();
             //check function
             if (!define.isPresent()) {
                 log.warn("function define is not find,functionName:{},txId:{}", vo.getFunctionName(), vo.getTxId());
@@ -148,9 +153,10 @@ import static io.stacs.nav.drs.api.exception.DappException.newError;
         vo.setAddress(address);
         vo.setPermissionNames(Lists.newArrayList(permission));
         //permission check
-        boolean res = blockChainFacade.checkPermission(vo).orElseThrow(newError(DAPP_NETWORK_COMMON_ERROR));
+        RespData<Boolean> res = blockChainHelper.post(CHECK_PERMISSION.getApi(), vo,
+            Boolean.class);// blockChainFacade.checkPermission(vo).orElseThrow(newError(DAPP_NETWORK_COMMON_ERROR));
 
-        if (!res) {
+        if (!res.isSuccessful() || !res.getData()) {
             log.warn("address:{} not has permission:{}", address, permission);
             throw new DappException(DappError.NO_PERMISSION_ERROR);
         }
