bf333dd369b8d4eb5f5e1a9f18f6028532f480e3
liuyu <11871958@qq.com>
2020-01-19 11:44:12 +0800

    properties use DB store

--- drs-boot/src/main/java/io/stacs/nav/drs/boot/service/PropertiesService.java
+++ drs-boot/src/main/java/io/stacs/nav/drs/boot/service/PropertiesService.java
@@ -3,8 +3,13 @@ package io.stacs.nav.drs.boot.service;
 import com.alibaba.fastjson.JSON;
 import io.stacs.nav.drs.boot.dao.SysConfDao;
 import io.stacs.nav.drs.boot.dao.po.SysConfPO;
+import io.stacs.nav.drs.service.config.DomainConfig;
+import io.stacs.nav.drs.service.config.DrsConfig;
+import io.stacs.nav.drs.service.utils.BeanConvertor;
+import io.stacs.nav.drs.service.utils.config.ConfigurationManager;
 import io.stacs.nav.drs.service.vo.ConfigVO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +18,19 @@ import org.springframework.stereotype.Service;
  * @description
  * @date 2020-01-19
  */
-@Service @Slf4j public class PropertiesService {
+@Service @Slf4j public class PropertiesService implements InitializingBean{
     private final static String _KEY = "PROPERTIES_KEY";
     @Autowired SysConfDao sysConfDao;
+    @Autowired ConfigurationManager manager;
+
+    @Override public void afterPropertiesSet() throws Exception {
+        ConfigVO vo = queryConfigVO();
+        if(vo != null){
+            //update for memory
+            manager.updateConfig(BeanConvertor.convertBean(vo.getDomainConfig(), DomainConfig.class));
+            manager.updateConfig(BeanConvertor.convertBean(vo.getDrsConfig(), DrsConfig.class));
+        }
+    }
     /**
      * save or update
      *
