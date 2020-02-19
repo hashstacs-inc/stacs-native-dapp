7c9841ef00aff5e028d5e22854791e2c00f99140
liuyu <11871958@qq.com>
2020-02-19 14:11:08 +0800

    fix bugs

--- dapp-core/src/main/java/io/stacs/nav/dapp/core/upgrade/UpgradeProcessor.java
+++ dapp-core/src/main/java/io/stacs/nav/dapp/core/upgrade/UpgradeProcessor.java
@@ -4,8 +4,11 @@ import com.alipay.sofa.ark.spi.service.ArkInject;
 import io.stacs.nav.drs.api.IUpgradeService;
 import io.stacs.nav.drs.api.model.UpgradeVersion;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -15,13 +18,12 @@ import java.io.IOException;
  * @description
  * @date 2020-02-14
  */
-@Component @Slf4j public class UpgradeProcessor {
+@Component @Slf4j public class UpgradeProcessor implements ApplicationContextAware{
     @Value("${spring.application.name}") private String appName;
     @ArkInject IUpgradeService upgradeService;
-    @Autowired IDappUpgradeHanlder dappUpgradeHanlder;
     @Autowired UpgradeDDLHandler upgradeDDLHandler;
     @Autowired UpgradeDMLHandler upgradeDMLHandler;
-
+    private ApplicationContext applicationContext;
     /**
      * process upgrade
      */
@@ -36,6 +38,7 @@ import java.io.IOException;
             log.info("[process]get upgradeVersion is null,appName:{}", appName);
             return;
         }
+        IDappUpgradeHanlder dappUpgradeHanlder = getDappHanlder();
         //check from dapp
         if (dappUpgradeHanlder != null && !dappUpgradeHanlder.shouldUpgrade(upgradeVersion)) {
             log.info("[process]Don't need upgrade,because shouldUpgrade return false,appName:{}", appName);
@@ -45,5 +48,24 @@ import java.io.IOException;
         log.info("[process]originalVersionCode:{}", originalVersionCode);
         upgradeDDLHandler.handler(originalVersionCode);
         upgradeDMLHandler.handler(originalVersionCode);
+    }
+
+    @Override public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    /**
+     * get dapp handler
+     *
+     * @return
+     */
+    private IDappUpgradeHanlder getDappHanlder(){
+        IDappUpgradeHanlder dappUpgradeHanlder = null;
+        try{
+            dappUpgradeHanlder = applicationContext.getBean(IDappUpgradeHanlder.class);
+        }catch(Throwable e){
+            log.warn("not have dapp upgrade handler");
+        }
+        return dappUpgradeHanlder;
     }
 }
